8d61a21ec3d44ee273959fc9cb91b2eae0c1d997
Garrett Ross <garrettross@gGarrettRoss.attlocal.net>
2024-02-13 08:08:29 -0600

    Working

--- mAlgos/arrays/areTheyEqual.js
+++ mAlgos/arrays/areTheyEqual.js
@@ -1,35 +1,37 @@
 
 
-function areTheyEqual(arr1, arr2) {
-    const obj1 = createObj(arr1)
-    const obj2 = createObj(arr2)
+// function areTheyEqual(arr1, arr2) {
+//     const obj1 = createObj(arr1)
+//     const obj2 = createObj(arr2)
 
-    if(Object.keys(obj1).length !== Object.keys(obj2).length) return false
+//     if(Object.keys(obj1).length !== Object.keys(obj2).length) return false
 
-    for(let key in obj2) {
-        if(!obj1[key] || obj1[key] !== obj2[key]) {
-            return false
-        }
-    }
-    return true
-}
+//     for(let key in obj2) {
+//         if(!obj1[key] || obj1[key] !== obj2[key]) {
+//             return false
+//         }
+//     }
+//     return true
+// }
 
-function createObj(arr) {
-    const obj = {}
+// function createObj(arr) {
+//     const obj = {}
 
-    arr.forEach((elem) => {
-        if(!obj[elem]) {
-            obj[elem] = 1
-        } else {
-            obj[elem]++
-        }
-    })
+//     arr.forEach((elem) => {
+//         if(!obj[elem]) {
+//             obj[elem] = 1
+//         } else {
+//             obj[elem]++
+//         }
+//     })
 
-    return obj
-}
+//     return obj
+// }
 
-const arr1 = [1,2,3,4,5,6,7,8]
-const arr2 = [2,3,1,4,5,8,7,12]
+// const arr1 = [2,4,1,3]
+// const arr2 = [1,2,3,4]
 
-const result = areTheyEqual(arr1, arr2)
-console.log(result)
+// const result = areTheyEqual(arr1, arr2)
+// console.log(result)
+
+console.log('working');
